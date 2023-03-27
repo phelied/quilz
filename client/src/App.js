b4ca3223539f1d7bfa7a6e17e4cef4766fb95634
kuueli <ophelie.diomar@epitech.eu>
2023-03-27 21:35:53 +0200

    add pages on the client part

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./pages/Layout";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import User from "./pages/User";
+import Layout from "./pages/layout";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import User from "./pages/user";
 import Error404 from "./pages/404";
 
 export default function App() {
