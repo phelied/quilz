2e20f476d926ee8a2755094b333fba94924a6760
kuueli <ophelie.diomar@epitech.eu>
2023-03-28 18:45:16 +0200

    remove unused css

--- client/src/pages/signin.js
+++ client/src/pages/signin.js
@@ -1,7 +1,4 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-import Character from "../assets/images/character-loginPage.png";
-import { Link } from "react-router-dom";
+import React from "react";
 import axios from 'axios';
 import AuthForm from "../components/authForm";
 
@@ -26,82 +23,10 @@ const SignIn = () => {
     }
 
     return (
-        <LoginContainer>
+        <>
             <AuthForm onSubmit={handleSubmit} />
-        </LoginContainer>
+        </>
     );
 };
-
-const LoginContainer = styled.main`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    color: white;
-    background-color: #7380ff;
-
-    & .container-image {
-        height: 50%;
-    }
-
-    & img {
-        width: 100%;
-    }
-`;
-
-const FormContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-
-`;
-const Introduction = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-
-    & h2 {
-        font-family: "Caraque-Melt";
-        font-weight: 800;
-        font-size: 3.75rem;
-        text-align: center;
-        margin: 0;
-    }
-`;
-
-const Form = styled.form`
-    display: flex;
-    flex-direction: column;
-    border: none;
-
-    & input {
-        height: 50px;
-        margin: 0.5rem 0;
-        padding: 0 1rem;
-        font-size: 1.25rem;
-        color: black;
-        border: 1px solid black;
-        border-radius: 0.75rem;
-    }
-
-    & input::placeholder {
-
-        color: black;
-    }
-
-    & button {
-        height: 50px;
-        border-radius: 0.75rem;
-        margin: 0.5rem 0;
-        padding: 0 1rem;
-        font-size: 1.25rem;
-        background-color: black;
-        color: white;
-        font-weight: 800;
-        cursor: pointer;
-    }
-`;
 
 export default SignIn;
